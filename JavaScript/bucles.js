35b6d3247298a550942286ab6f63fbd1acb96511
Jessica Olivares <jessicaolivares863@gmail.com>
2024-04-22 16:57:12 -0300

    hice el ejercicio 6 pero me falta poner mas pokemones

---
+++ JavaScript/bucles.js
@@ -0,0 +1,12 @@
+//bucles y arrays
+const array = [2,3,'pepe'];
+
+//while(del ingles mientras)
+while(array.length < 1){
+   console.log('test');
+}
+//bucle for
+for(let i = 0; i < array.length; i++) {//siempre adentro del for van a usar let
+    console.log(i)
+    console.log('posicion',i,'valor',array(i))
+}
---
+++ JavaScript/primer.js
@@ -0,0 +1,17 @@
+var numero = 1; //asignamos el valor a una variable tipo number
+var palabra = 'palabra'//asignamos el valor a una variable tipo string
+
+//definimos variables con la palabra reservada let para evitar ser llamada en cualquier lado del codigo 
+let numero2 = 2; //
+let palabra2 = 'palabra 2'; //
+
+//definimos constante con la palabra  reservada const
+const numCostante = 4; //siempre que sepan que no van a mutar 
+
+//ejemplo
+if(numero > 8){
+    let numero3 =3;
+}
+
+console.log('numero',numero2);
+///////////////////////////////////////////////
