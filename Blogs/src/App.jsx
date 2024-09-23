dde251b12a3919a861b7aadb209dc81e7493218f
Jessica Olivares <jessicaolivares863@gmail.com>
2024-09-23 15:38:11 -0300

    agregamos el boton de iniciar sesion y salir de github,ademas de mejorar algunas cosas de mi codigo

--- Blogs/src/App.jsx
+++ Blogs/src/App.jsx
@@ -10,28 +10,37 @@ export default function App() {
   const [user, setUser] = useState(null);
 
   useEffect(() => {
-    const getSession = async () => {
-      ///destructuracion
-      const { data, error } = await supabase.auth.getSession();
-      if (error) {
-        console.log(error);
-      } else {
-        setUser(data?.session?.user);
-      }
-    };
+    const session = supabase.auth.getSession();
+    setUser(session?.user);
 
-    getSession();
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((event, session) => {
+      switch (event) {
+        case "SIGNED_IN":
+          setUser(session?.user);
+          break;
+        case "SIGNED_OUT":
+          setUser(null);
+          break;
+        default:
+          console.log("caso no estimado");
+      }
+    });
+
+    return () => {
+      subscription.unsubscribe();
+    };
   }, []);
 
   const handleLogin = async () => {
-    const { error, data } = await supabase.auth.signInWithOAuth({
+    await supabase.auth.signInWithOAuth({
       provider: "github",
     });
-    if (error) {
-      console.log(error);
-    } else {
-      console.log(data);
-    }
+  };
+
+  const handleLogout = async () => {
+    await supabase.auth.signOut();
   };
 
   return (
@@ -42,67 +51,74 @@ export default function App() {
         </div>
       </div>
 
-      <div className="centrar2"data-aos="zoom-in">
-      <button onClick={handleLogin}>inicio sesión</button>
-      <Footer />
+      <div className="centrar2" data-aos="zoom-in">
+        {user ? (
+          <div>
+            <h2>Authenticated</h2>
+            <button onClick={handleLogout}>logout</button>
+          </div>
+        ) : (
+          <button onClick={handleLogin}>login Github</button>
+        )}
       </div>
-    
-    <div className="grilla2" >
-      <div className="borde letra " data-aos="flip-left">
-        <Post
-          titulo={"Mi Blogs"}
-          description={"yo a la night"}
-          link={"./src/public/jkkk.jpg"}
-          parrafo={"mi cantante favorito"}
-        />
-      </div>
+      <div className="grilla2">
+        <div className="borde letra " data-aos="flip-left">
+          <Post
+            titulo={"Mi Blogs"}
+            description={"yo a la night"}
+            link={"./src/public/jkkk.jpg"}
+            parrafo={"mi cantante favorito"}
+          />
+        </div>
 
-      <div className=" borde letra" data-aos="flip-right">
-        <Post
-          titulo={"Mi Blogs"}
-          description={"yo a la night"}
-          link={"./src/public/im1.jpg"}
-          parrafo={"deje pasar el colectivo y no llego despues de 20 mn"}
-        />
-      </div>
+        <div className=" borde letra" data-aos="flip-right">
+          <Post
+            titulo={"Mi Blogs"}
+            description={"yo a la night"}
+            link={"./src/public/im1.jpg"}
+            parrafo={"deje pasar el colectivo y no llego despues de 20 mn"}
+          />
+        </div>
 
-      <div className="borde letra" data-aos="flip-right">
-        <Post
-          titulo={"Mi Blogs"}
-          description={"yo a la night"}
-          link={"./src/public/im2.jpg"}
-          parrafo={"fui abrigada para que hiciera calor, era ese"}
-        />
-      </div>
+        <div className="borde letra" data-aos="flip-right">
+          <Post
+            titulo={"Mi Blogs"}
+            description={"yo a la night"}
+            link={"./src/public/im2.jpg"}
+            parrafo={"fui abrigada para que hiciera calor, era ese"}
+          />
+        </div>
 
-      <div className="borde letra" data-aos="flip-left">
-        <Post
-          titulo={"Mi Blogs"}
-          description={"yo a la night"}
-          link={"./src/public/im3.jpg"}
-          parrafo={"se me cayo una araña al lado mio"}
-        />
-      </div>
+        <div className="borde letra" data-aos="flip-left">
+          <Post
+            titulo={"Mi Blogs"}
+            description={"yo a la night"}
+            link={"./src/public/im3.jpg"}
+            parrafo={"se me cayo una araña al lado mio"}
+          />
+        </div>
 
-      <div className="borde letra" data-aos="flip-right">
-        <Post
-          titulo={"Mi Blogs"}
-          description={"yo a la night"}
-          link={"./src/public/im4.jpg"}
-          parrafo={"yo"}
-        />
-      </div>
+        <div className="borde letra" data-aos="flip-right">
+          <Post
+            titulo={"Mi Blogs"}
+            description={"yo a la night"}
+            link={"./src/public/im4.jpg"}
+            parrafo={"yo"}
+          />
+        </div>
 
-      <div className="borde letra" data-aos="flip-right">
-        <Post
-          titulo={"Mi Blogs"}
-          description={"yo a la night"}
-          link={"./src/public/im5.jpg"}
-          parrafo={"xc"}
-        />
+        <div className="borde letra" data-aos="flip-right">
+          <Post
+            titulo={"Mi Blogs"}
+            description={"yo a la night"}
+            link={"./src/public/im5.jpg"}
+            parrafo={"xc"}
+          />
+        </div>
+        <div className="centrar3">
+          <Footer />
+        </div>
       </div>
-
-    </div>
     </>
   );
 }
